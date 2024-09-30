aee7dce4c7830fc64ed019a2da8f0a73b5b2ed05
Abdul Fatah <fatah03111995@gmail.com>
2024-09-30 11:36:42 +0700

    feat : setup
    + orientation
    + status bar color
    + screen util

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,32 @@
+import 'package:challange_submission/core/bloc/quiz/quiz_bloc.dart';
+import 'package:challange_submission/core/bloc/slider/slider_cubit.dart';
+import 'package:challange_submission/ui/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// ALLOWED ORIENTATION
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  /// TRANSPARENT STATUS BAR
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider(
+        create: (context) => SliderCubit(),
+      ),
+      BlocProvider(
+        create: (context) => QuizBloc(),
+      ),
+    ],
+    child: const MainApp(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +34,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return const ScreenUtilInit(
+      child: MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()),
     );
   }
 }
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:challange_submission/core/theme/text_styles.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +6,13 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'This is HOMEPAGE',
+          style: TextStyles.m,
+        ),
+      ),
+    );
   }
 }
